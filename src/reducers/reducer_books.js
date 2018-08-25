f92dc15231d67ba9bb39fd4ef98710e6e6d620a5
Nitesh.ldd <nitesh.ldd@gmail.com>
2018-08-25 21:57:34 +0530

    Version first able to show booklist

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function () {
+    return [
+        {titile: 'Javscript : The good part' },
+        {titile: 'Node Js: Server book'},
+        {titile: 'pyhton: new book'},
+        {titile: 'AWS: Cloud book'},
+        {titile: 'React: Ui Design'},
+        
+    ]
+}
